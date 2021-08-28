7fe0b74e4b0dbb612ac8859aaf816dd0b0fb6630
soumik00 <78477427+soumik00@users.noreply.github.com>
2021-08-28 21:25:33 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+// const apple =  #F8FF
+
+alert("Click the apple  logo");
